31614584e83e6a0c9fb3c3d08d78811b91343fd9
Nicolas Dickreuter <dickreuter@gmail.com>
2023-10-27 22:32:54 +0100

    remove .env

--- src/helper/Constants.tsx
+++ src/helper/Constants.tsx
@@ -1,5 +1,5 @@
-// export const API_URL = import.meta.env.VITE_REACT_APP_API_URL || 'betfair-bot.com:7779';
-export const API_URL = 'betfair-bot.com:7779';
+export const API_URL = import.meta.env.VITE_REACT_APP_API_URL || 'betfair-bot.com:7779';
+// export const API_URL = 'betfair-bot.com:7779';
 
 
 export const DATA_ATTRIBUTES = [
